9e1a811751dac7f6b9a8650e58633901498fdc83
Volovikov <qylikys@gmail.com>
2023-08-14 20:02:36 +0300

    task3_update

--- Task3.py
+++ Task3.py
@@ -1,12 +1,12 @@
 arr = list(map(int, input().split()))
 n = len(arr)
-ans = []
-for i in range(n):
+ans = [None] * n
+ans[0] = arr[0]
+for i in range(1, n):
     cur = arr[i]
-    pos = 0
-    ans.append(cur)
-    for j in range(len(ans)):
-        if cur >= ans[j]:
-            pos = j
-    ans[pos] = cur
+    pos = i - 1
+    while pos >= 0 and ans[pos] > cur:
+        ans[pos + 1] = ans[pos]
+        pos -= 1
+    ans[pos + 1] = cur
 print(ans)
