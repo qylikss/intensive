1a21d304af58a70331221dcdcb4c4f81abe69c4e
Volovikov <qylikys@gmail.com>
2023-08-14 19:30:19 +0300

    task3_update

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 arr = list(map(int, input().split()))
 n = len(arr)
-ans = []
-ans_len = 0
 for i in range(n):
     cur = arr[i]
-    pos = ans_len
-    for j in range(len(ans)):
-        if cur <= ans[j]:
-            pos = j
-            break
-    ans.insert(pos, cur)
-    ans_len += 1
-print(ans)
+    pos = 0
+    while pos < i and arr[pos] <= arr[i]:
+        pos += 1
+    for j in range(pos, i):
+        arr[j], arr[i] = arr[i], arr[j]
+print(arr)
